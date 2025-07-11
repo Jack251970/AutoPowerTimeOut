59b2fb5b86078c17899cc863c9ccdc2ae42249ff
Jack251970 <1160210343@qq.com>
2025-07-11 12:17:02 +0800

    Fix recommended text issue

--- TimeOutModel.cs
+++ TimeOutModel.cs
@@ -10,13 +10,12 @@ public class TimeOutModel
     public TimeOutLevel Value { get; private init; }
     public string Display { get; set; } = string.Empty;
     
-    private const string RecommendedFormat = "{0} (Recommended)";
     public static List<TimeOutModel> GetValues(bool pluggedIn)
     {
         var values = Enum.GetValues<TimeOutLevel>().Cast<TimeOutLevel>().Select(v => new TimeOutModel
         {
             Value = v,
-            Display = GetDescriptionAttr(v)
+            Display = GetDescriptionAttr(v, pluggedIn)
         });
         // Put the "Never" option at the end
         var never = values.FirstOrDefault(v => v.Value == TimeOutLevel.Never);
@@ -29,39 +28,31 @@ public class TimeOutModel
             _ = values.Append(new TimeOutModel
             {
                 Value = TimeOutLevel.Never,
-                Display = GetDescriptionAttr(TimeOutLevel.Never)
+                Display = GetDescriptionAttr(TimeOutLevel.Never, pluggedIn)
             });
-        }
-        if (pluggedIn)
-        {
-            // 5 minutes is recommended for plugged in
-            foreach (var item in values)
-            {
-                if (item.Value == TimeOutLevel.FiveMinutes)
-                {
-                    item.Display = string.Format(RecommendedFormat, item.Display);
-                }
-            }
-        }
-        else
-        {
-            // 3 minutes is recommended for on battery
-            foreach (var item in values)
-            {
-                if (item.Value == TimeOutLevel.ThreeMinutes)
-                {
-                    item.Display = string.Format(RecommendedFormat, item.Display);
-                }
-            }
         }
         return [.. values];
     }
 
-    private static string GetDescriptionAttr(Enum value)
+    private const string RecommendedFormat = "{0} (Recommended)";
+    private static string GetDescriptionAttr(TimeOutLevel value, bool pluggedIn)
     {
         var field = value.GetType().GetField(value.ToString());
         var attributes = (DescriptionAttribute[])field?.GetCustomAttributes(typeof(DescriptionAttribute), false)!;
-        return attributes is { Length: > 0 } ? attributes[0].Description : value.ToString();
+        var description = attributes is { Length: > 0 } ? attributes[0].Description : value.ToString();
+        // 5 minutes is recommended for plugged in
+        if (pluggedIn && value == TimeOutLevel.FiveMinutes)
+        {
+            return string.Format(RecommendedFormat, description);
+        }
+        else if (!pluggedIn && value == TimeOutLevel.ThreeMinutes)
+        {
+            return string.Format(RecommendedFormat, description);
+        }
+        else
+        {
+            return description;
+        }
     }
 }
 
