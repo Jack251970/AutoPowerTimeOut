c1284826b2140b7f42d5cae15271de1d68fcba2f
Jack251970 <1160210343@qq.com>
2025-07-11 09:45:35 +0800

    Add basic UI

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,14 +1,13 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
+using System.Windows.Threading;
 
-namespace AutoPowerTimeOut
+namespace AutoPowerTimeOut;
+
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
     {
+        MessageBox.Show(e.Exception.ToString());
+        e.Handled = true;
     }
-
 }
