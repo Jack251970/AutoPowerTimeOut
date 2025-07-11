8bd27aaca2bf015e2ba0d62a42fae2045ddd4612
Jack251970 <1160210343@qq.com>
2025-07-11 10:15:42 +0800

    Storage settings

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,21 +1,106 @@
-﻿using System.Windows;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
 
 namespace AutoPowerTimeOut;
 
-public partial class MainWindow : Window
+public partial class MainWindow : Window, INotifyPropertyChanged
 {
     public List<TimeOutModel> PluggedInItemSource { get; set; } = TimeOutModel.GetValues(true);
     public List<TimeOutModel> OnBatteryItemSource { get; set; } = TimeOutModel.GetValues(false);
 
-    public TimeOutLevel PluggedInScreen { get; set; } = TimeOutLevel.FiveMinutes;
-    public TimeOutLevel PluggedInSleep { get; set; } = TimeOutLevel.FiveMinutes;
+    private TimeOutLevel _pluggedInScreen;
+    public TimeOutLevel PluggedInScreen
+    {
+        get => _pluggedInScreen;
+        set
+        {
+            if (_pluggedInScreen != value)
+            {
+                _pluggedInScreen = value;
+                OnPropertyChanged();
+            }
+        }
+    }
 
-    public TimeOutLevel OnBatteryScreen { get; set; } = TimeOutLevel.ThreeMinutes;
-    public TimeOutLevel OnBatterySleep { get; set; } = TimeOutLevel.ThreeMinutes;
+    private TimeOutLevel _pluggedInSleep;
+    public TimeOutLevel PluggedInSleep
+    {
+        get => _pluggedInSleep;
+        set
+        {
+            if (_pluggedInSleep != value)
+            {
+                _pluggedInSleep = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    private TimeOutLevel _onBatteryScreen;
+    public TimeOutLevel OnBatteryScreen
+    {
+        get => _onBatteryScreen;
+        set
+        {
+            if (_onBatteryScreen != value)
+            {
+                _onBatteryScreen = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    private TimeOutLevel _onBatterySleep;
+    public TimeOutLevel OnBatterySleep
+    {
+        get => _onBatterySleep;
+        set
+        {
+            if (_onBatterySleep != value)
+            {
+                _onBatterySleep = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    private readonly Settings _settings;
 
     public MainWindow()
     {
-        Application.Current.MainWindow = this;
+        _settings = Settings.Load();
+        PluggedInScreen = _settings.PluggedInScreen;
+        PluggedInSleep = _settings.PluggedInSleep;
+        OnBatteryScreen = _settings.OnBatteryScreen;
+        OnBatterySleep = _settings.OnBatterySleep;
         InitializeComponent();
+        PropertyChanged += MainWindow_PropertyChanged;
+    }
+
+    private void MainWindow_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        switch (e.PropertyName)
+        {
+            case nameof(PluggedInScreen):
+                _settings.PluggedInScreen = PluggedInScreen;
+                break;
+            case nameof(PluggedInSleep):
+                _settings.PluggedInSleep = PluggedInSleep;
+                break;
+            case nameof(OnBatteryScreen):
+                _settings.OnBatteryScreen = OnBatteryScreen;
+                break;
+            case nameof(OnBatterySleep):
+                _settings.OnBatterySleep = OnBatterySleep;
+                break;
+        }
+        _settings.Save();
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
---
+++ Settings.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Text.Json;
+
+namespace AutoPowerTimeOut;
+
+public class Settings
+{
+    public TimeOutLevel PluggedInScreen { get; set; } = TimeOutLevel.FiveMinutes;
+    public TimeOutLevel PluggedInSleep { get; set; } = TimeOutLevel.FiveMinutes;
+
+    public TimeOutLevel OnBatteryScreen { get; set; } = TimeOutLevel.ThreeMinutes;
+    public TimeOutLevel OnBatterySleep { get; set; } = TimeOutLevel.ThreeMinutes;
+
+    private static readonly string SettingsPath = Path.Combine(AppContext.BaseDirectory, "Settings.json");
+
+    public static Settings Load()
+    {
+        if (!File.Exists(SettingsPath))
+        {
+            return new Settings();
+        }
+        var json = File.ReadAllText(SettingsPath);
+        return JsonSerializer.Deserialize<Settings>(json) ?? new Settings();
+    }
+
+    public void Save()
+    {
+        var json = JsonSerializer.Serialize(this);
+        File.WriteAllText(SettingsPath, json);
+    }
+}
