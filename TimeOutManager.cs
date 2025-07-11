db802139803c07044153eebb8c7246a2504adc1f
Jack251970 <1160210343@qq.com>
2025-07-11 09:49:46 +0800

    Add time-out maneger

---
+++ TimeOutManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.InteropServices;
+
+namespace AutoPowerTimeOut;
+
+public static class TimeOutManager
+{
+    public static void SetDisplayAndSleepTimeout(uint acDisplayMinutes, uint dcDisplayMinutes,
+                                                 uint acSleepMinutes, uint dcSleepMinutes)
+    {
+        var pActiveScheme = IntPtr.Zero;
+        try
+        {
+            var res = Win32Helper.PowerGetActiveScheme(IntPtr.Zero, out pActiveScheme);
+            if (res != 0)
+            {
+                throw new Exception($"PowerGetActiveScheme failed: {res}");
+            }
+
+            var activeScheme = Marshal.PtrToStructure<Guid>(pActiveScheme);
+
+            // Set AC values
+            _ = Win32Helper.PowerWriteACValueIndex(IntPtr.Zero, activeScheme, Win32Helper.GUID_VIDEO_SUBGROUP, Win32Helper.GUID_VIDEO_TIMEOUT, acDisplayMinutes * 60);
+            _ = Win32Helper.PowerWriteACValueIndex(IntPtr.Zero, activeScheme, Win32Helper.GUID_SLEEP_SUBGROUP, Win32Helper.GUID_SLEEP_IDLE, acSleepMinutes * 60);
+
+            // Set DC values
+            _ = Win32Helper.PowerWriteDCValueIndex(IntPtr.Zero, activeScheme, Win32Helper.GUID_VIDEO_SUBGROUP, Win32Helper.GUID_VIDEO_TIMEOUT, dcDisplayMinutes * 60);
+            _ = Win32Helper.PowerWriteDCValueIndex(IntPtr.Zero, activeScheme, Win32Helper.GUID_SLEEP_SUBGROUP, Win32Helper.GUID_SLEEP_IDLE, dcSleepMinutes * 60);
+
+            // Commit changes
+            _ = Win32Helper.PowerSetActiveScheme(IntPtr.Zero, activeScheme);
+        }
+        finally
+        {
+            if (pActiveScheme != IntPtr.Zero)
+            {
+                Win32Helper.LocalFree(pActiveScheme);
+            }
+        }
+    }
+}
---
+++ Win32Helper.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.InteropServices;
+
+namespace AutoPowerTimeOut;
+
+internal class Win32Helper
+{
+    // Import required power profile functions
+    [DllImport("powrprof.dll", SetLastError = true)]
+    public static extern uint PowerGetActiveScheme(IntPtr UserRootPowerKey, out IntPtr ActivePolicyGuid);
+
+    [DllImport("powrprof.dll", SetLastError = true)]
+    public static extern uint PowerSetActiveScheme(IntPtr UserRootPowerKey, [MarshalAs(UnmanagedType.LPStruct)] Guid SchemeGuid);
+
+    [DllImport("powrprof.dll", SetLastError = true)]
+    public static extern uint PowerWriteACValueIndex(IntPtr RootPowerKey, [MarshalAs(UnmanagedType.LPStruct)] Guid SchemeGuid,
+        [MarshalAs(UnmanagedType.LPStruct)] Guid SubGroupOfTimeOutSettingsGuid, [MarshalAs(UnmanagedType.LPStruct)] Guid PowerSettingGuid,
+        uint AcValueIndex);
+
+    [DllImport("powrprof.dll", SetLastError = true)]
+    public static extern uint PowerWriteDCValueIndex(IntPtr RootPowerKey, [MarshalAs(UnmanagedType.LPStruct)] Guid SchemeGuid,
+        [MarshalAs(UnmanagedType.LPStruct)] Guid SubGroupOfTimeOutSettingsGuid, [MarshalAs(UnmanagedType.LPStruct)] Guid PowerSettingGuid,
+        uint DcValueIndex);
+
+    // Release the pointer returned by PowerGetActiveScheme
+    [DllImport("kernel32.dll")]
+    public static extern void LocalFree(IntPtr hMem);
+
+    // GUIDs
+    public static Guid GUID_VIDEO_SUBGROUP = new("7516b95f-f776-4464-8c53-06167f40cc99");
+    public static Guid GUID_VIDEO_TIMEOUT = new("3c0bc021-c8a8-4e07-a973-6b14cbcb2b7e");
+
+    public static Guid GUID_SLEEP_SUBGROUP = new("238C9FA8-0AAD-41ED-83F4-97BE242C8F20");
+    public static Guid GUID_SLEEP_IDLE = new("29f6c1db-86da-48c5-9fdb-f2b67b1f44da");
+}
